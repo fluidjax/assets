6331902a9e382775389fa0911e31843d29f0695f
Christopher Morris <chris@morris.net>
2020-01-13 11:24:24 +0000

    Re-arrange docs

--- libs/assets/trusteegroup_test.go
+++ libs/assets/trusteegroup_test.go
@@ -13,59 +13,56 @@ import (
 
 func Test_TrusteeGroup(t *testing.T) {
 
-	for i := 0; i < 40; i++ {
-		fmt.Println(i)
-		store := NewMapstore()
-		idInitiator, idT1, idT2, idT3 := SetupIDDocs(store)
+	store := NewMapstore()
+	idInitiator, idT1, idT2, idT3 := SetupIDDocs(store)
 
-		_ = idInitiator
+	_ = idInitiator
 
-		testName := "ABC!23"
-		testDescription := "ZXC#@!"
-		i, err := NewIDDoc(testName)
-		assert.Nil(t, err, "Error should be nil")
-		i.Sign(i)
-		i.store = store
-		i.Save()
+	testName := "ABC!23"
+	testDescription := "ZXC#@!"
+	i, err := NewIDDoc(testName)
+	assert.Nil(t, err, "Error should be nil")
+	i.Sign(i)
+	i.store = store
+	i.Save()
 
-		w, err := NewTrusteeGroup(i)
-		expression := "t1 + t2 + t3 > 1 "
-		participants := &map[string][]byte{
-			"t1": idT1.Key(),
-			"t2": idT2.Key(),
-			"t3": idT3.Key(),
-		}
-
-		w.ConfigureTrusteeGroup(expression, participants, testDescription)
-
-		b := proto.NewBuffer(nil)
-		b.SetDeterministic(true)
-		b.Marshal(w.Asset)
-		msg1, _ := b.DecodeRawBytes(true)
-		res1 := sha256.Sum256(msg1)
-		fmt.Println(hex.EncodeToString(res1[:]))
-
-		w.Sign(i)
-
-		assert.NotNil(t, w.PBSignedAsset.Signature, "Signature is empty")
-		res, err := w.Verify(i)
-
-		c := proto.NewBuffer(nil)
-		c.SetDeterministic(true)
-		c.Marshal(w.Asset)
-		msg2, _ := c.DecodeRawBytes(true)
-		res2 := sha256.Sum256(msg2)
-		fmt.Println(hex.EncodeToString(res2[:]))
-
-		assert.Nil(t, err, "Error should be nil")
-		assert.True(t, res, "Verify should be true")
-		w.Save()
-
-		retrieved, err := Load(i.store, w.Key())
-		retrievedTrusteeGroup := retrieved.Asset.GetTrusteeGroup()
-
-		assert.Equal(t, testDescription, retrievedTrusteeGroup.TrusteeGroup.Description, "Load/Save failed")
+	w, err := NewTrusteeGroup(i)
+	expression := "t1 + t2 + t3 > 1 "
+	participants := &map[string][]byte{
+		"t1": idT1.Key(),
+		"t2": idT2.Key(),
+		"t3": idT3.Key(),
 	}
+
+	w.ConfigureTrusteeGroup(expression, participants, testDescription)
+
+	b := proto.NewBuffer(nil)
+	b.SetDeterministic(true)
+	b.Marshal(w.Asset)
+	msg1, _ := b.DecodeRawBytes(true)
+	res1 := sha256.Sum256(msg1)
+	fmt.Println(hex.EncodeToString(res1[:]))
+
+	w.Sign(i)
+
+	assert.NotNil(t, w.PBSignedAsset.Signature, "Signature is empty")
+	res, err := w.Verify(i)
+
+	c := proto.NewBuffer(nil)
+	c.SetDeterministic(true)
+	c.Marshal(w.Asset)
+	msg2, _ := c.DecodeRawBytes(true)
+	res2 := sha256.Sum256(msg2)
+	fmt.Println(hex.EncodeToString(res2[:]))
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.True(t, res, "Verify should be true")
+	w.Save()
+
+	retrieved, err := Load(i.store, w.Key())
+	retrievedTrusteeGroup := retrieved.Asset.GetTrusteeGroup()
+
+	assert.Equal(t, testDescription, retrievedTrusteeGroup.TrusteeGroup.Description, "Load/Save failed")
 }
 
 //Tests to ensure objects are determinsitically serialized - something protobuffers by default does not guarantee
